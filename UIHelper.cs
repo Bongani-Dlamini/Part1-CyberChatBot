061fa637c82a38058b67994922aa2987a5ef06f7
Bongani Dlamini <st10451243@myemeris.edu.za>
2026-03-30 19:35:37 +0200

    added the ascii logo

--- UIHelper.cs
+++ UIHelper.cs
@@ -20,5 +20,22 @@ namespace Part1_CyberChatBot
                 Console.WriteLine("Audio file 'Greeting.wav' not found.");
             }
         }
+
+        // This is the ascii logo.
+        public void ShowAsciiArt()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║  ____       _               ____ _           _   ____        _                                                                           ║");
+            Console.WriteLine("║ / ___| ___ | |__   ___ _ __/ ___| |__   __ _| |_ | __ )  ___ | |_                                                                             ║");
+            Console.WriteLine("║| |    / _ \\| '_ \\ / _ \\ '__| |   | '_ \\ / _` | __||  _ \\ / _ \\| __|                     ║");
+            Console.WriteLine("║| |___| (_) | |_) |  __/ |  | |___| | | | (_| | |_ | |_) | (_) | |_                                                                       ║");
+            Console.WriteLine("║ \\____|\\___/|_.__/ \\___|_|   \\____|_| |_|\\__,_|\\__||____/ \\___/ \\__|                                                                     ║");
+            Console.WriteLine("╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
+
+            Console.ResetColor();
+            Console.WriteLine();
+        }
     }
 }
