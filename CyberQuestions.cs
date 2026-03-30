d08ae9f59e0b93bcbcf184027d180eeea667690b
Bongani Dlamini <st10451243@myemeris.edu.za>
2026-03-30 18:39:41 +0200

    Added LoadQuestions method to initialize cybersecurity questions and answers using parallel arrays

---
+++ CyberQuestions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part1_CyberChatBot
+{
+    internal class CyberQuestions
+    {
+        // Used parallel to store the questions and the answers
+        public string[] Questions;
+        public string[] Answers;
+
+        public void LoadQuestions()
+        {
+            // The 30 Cybersecurity Questions/Answers that the user will be able to ask the bot.
+            Questions = new string[]
+            {
+                "antivirus",           
+                "ransomware",           
+                "spyware",             
+                "keyloggers",           
+                "trojan horse",        
+                "viruses",              
+                "malware",              
+                "phishing emails",      
+                "phishing",             
+                "two-factor auth",      
+                "strong passwords",    
+                "passwords",            
+                "firewalls",            
+                "vpn",                  
+                "social engineering",   
+                "identity theft",       
+                "encryption",           
+                "data breaches",        
+                "cyberbullying",        
+                "https",                
+                "public wi-fi",         
+                "ddos attacks",         
+                "personal information", 
+                "cookies",              
+                "shoulder surfing",     
+                "brute force",          
+                "data backups",         
+                "what to do if hacked", 
+                "online scams",         
+                "software updates"      
+            };
+
+        }
+    }
+}
